bc0b6e5c5411456f758bea01e77ae32407814229
Masaki Abe <abem@dc4.so-net.ne.jp>
2017-07-21 10:40:54 +0900

    コルーチンの呼び出しタイミングがフレーム単位かどうかを確認してみた。 見た目上はフレーム単位。少なくとも1/60秒以下では無さそう。計測はしていないが、今はその認識で十分。

--- Assets/Rotate/RotateX.cs
+++ Assets/Rotate/RotateX.cs
@@ -20,6 +20,10 @@ public class RotateX : MonoBehaviour {
 
             Debug.Log("X回転終了");
         }
+
+		//	コルーチン実行中も Update() の処理は実行を続ける
+		//	yield で関数のワークが保持されるので、それをフックして別途呼び出している？
+		//	デバッガで確認すると Update() も コルーチンもスレッドID 1から呼ばれていた
         proc_count++;
         if( (proc_count % 60) == 0 )
         {
@@ -34,7 +38,9 @@ public class RotateX : MonoBehaviour {
             yield return new WaitForSeconds(3);
             for (int i = 0; i < 180; i++)
             {
-                yield return new WaitForSeconds(0.016f);
+				//	MonoBehaviourのどこかにフックされて毎フレーム呼ばれている？
+                //yield return new WaitForSeconds(0.016f);
+				yield return null;
                 transform.Rotate(1, 0, 0);
             }
         }    
