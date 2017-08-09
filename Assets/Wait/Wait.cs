1a4edbdd0f9938b80048264aba3f4d9fd065d6cb
Masaki Abe <abem@dc4.so-net.ne.jp>
2017-08-09 12:10:09 +0900

    コルーチン内で終了を待つ処理を作成。 フラグを外部から書き換えられた場合の確認。 最適化などで消えたりしない…？ 最終的な最適化はプラットフォームに依存するので確認には至っていない。

---
+++ Assets/Wait/Wait.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Wait : MonoBehaviour {
+
+	GameObject textObj;
+	GameObject buttonObj;
+
+	// Use this for initialization
+	void Start () {
+		textObj = GameObject.Find("MessageText");
+		buttonObj = GameObject.Find("Button");
+		buttonObj.GetComponent<Button>().onClick.AddListener(onClick);
+	
+		StartCoroutine(waitProc());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	bool isEnd;
+	IEnumerator waitProc()
+	{
+		string text = "処理中";
+
+		int count = 0;
+
+		Text tcomp = textObj.GetComponent<Text>();
+
+		isEnd = false;
+		while( false == isEnd )
+		{
+			count++;
+
+			int plus = (count / 15) % 6;
+			string t = text;
+
+			for(int i=0; i<plus; i++)
+			{
+				t += ".";
+			}
+
+			tcomp.text = t;
+			yield return null;
+		}
+	
+		tcomp.text = "処理終了";
+	}
+
+
+	void onClick()
+	{
+		Debug.Log("ボタン押された");
+		isEnd = true;
+	}
+
+}
