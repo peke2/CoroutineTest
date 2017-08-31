7a8137f51ea6e84a4ea46b4798eb7f28c24542d4
Masaki Abe <abem@dc4.so-net.ne.jp>
2017-08-31 19:49:02 +0900

    コルーチンが動いているところを外部から破棄したらどうなるかの確認を追加。 GameObjectに紐付いているようで、破棄した時点で処理が止まった。

---
+++ Assets/Destroy/Routine.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Routine : MonoBehaviour {
+
+	Text text;
+
+	// Use this for initialization
+	void Start () {
+		text = transform.Find("Text").GetComponent<Text>();
+		StartCoroutine(counter());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator counter()
+	{
+		int  time = 0;
+
+		while(true)
+		{
+			text.text = time.ToString();
+			yield return new WaitForSeconds(1);
+			Debug.Log(time);
+			time += 1;
+		}
+	}
+}
---
+++ Assets/Destroy/TargetDestroy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TargetDestroy : MonoBehaviour {
+
+	//	Coroutineが動いている対象を破棄したらどうなるか？
+	//	【結果】オブジェクトが破棄されてCoroutineの処理も止まる
+	//	独立した処理としてCoroutineを動かしている時に、所属元のMonoBehaviourを破棄すると止まる
+	//	処理が動いている間は残り続けるかと思ったけど、そんなことは無かった
+
+	public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		Button btn = transform.GetComponent<Button>();
+		btn.onClick.AddListener(onButtonClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void onButtonClick()
+	{
+		if( null == target )	return;
+
+		GameObject.Destroy(target);
+		target = null;
+	}
+}
